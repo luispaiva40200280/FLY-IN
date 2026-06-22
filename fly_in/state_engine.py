b90900256a3c46d9df0e3e4c1a80423a7ca2a964
luispaiva40200280 <luispaiva8936@gmail.com>
2026-06-23 00:26:07 +0100

    refractor the state engine

--- fly_in/state_engine.py
+++ fly_in/state_engine.py
@@ -4,6 +4,7 @@ and docstrings explaining what each part does
 """
 from models.network import Map
 from models.constants import DroneState, ZoneType
+from models.data import Drone
 
 
 class SimulationError(Exception):
@@ -22,40 +23,8 @@ class StateEngine:
     # =========================================================
     # PHASE 1: THE DEPARTURE (Freeing Capacity)
     # ========================================================
-    def free_nodes_capacity(self) -> None:
-        pass
-
-    # =========================================================
-    # PHASE 2: THE VALIDATION (Enforcing Physics)
-    # =========================================================
-    def validate_move(self) -> None:
-        pass
-
-    def execute_turn(self, moves: dict[str, str]) -> None:
-        """
-        proposed_moves:
-        A dictionary mapping {Drone: TargetZone} for this turn.
-        If a drone is waiting, TargetZone == drone.current_zone.
-        """
-        from collections import deque
-        self.turn_counter += 1
-        # this is for checking the state of the map in every turn
-        # and see the zones that have and how may drones are in there
-        nodes_occupency: dict[str, int] = {zone.name: 0
-                                           for zone in self.map.zones.values()}
-        for drone in self.map.drones:
-            if drone.state in (DroneState.ARRIVED, DroneState.WAITING):
-                nodes_occupency[drone.current_zone] += 1
-
-        # 2. Initialize CONNECTION buffer (stateless, guaranteed empty at
-        # start of turn) Using the frozenset as the key matches your
-        # network.py architecture
-        current_connection_usage: dict[frozenset[str], int] = {
-            f_set: 0 for f_set in self.map.connections.keys()
-        }
-        # =========================================================
-        # PHASE 1: THE DEPARTURE (Freeing Capacity)
-        # =========================================================
+    def _free_nodes_capacity(self, nodes_occupency: dict[str, int],
+                             moves: dict[str, str]) -> None:
         for drone in self.map.drones:
             if drone.state == DroneState.IN_TRANSIT:
                 continue
@@ -63,12 +32,17 @@ class StateEngine:
             if target != drone.current_zone:
                 nodes_occupency[drone.current_zone] -= 1
 
+    # =========================================================
+    # PHASE 2: THE VALIDATION (Enforcing Physics)
+    # =========================================================
+    def _validate_moves(self, nodes_occupency: dict[str, int], moves:
+                        dict[str, str]) -> list[tuple[Drone, str, DroneState]]:
+        current_connection_usage: dict[frozenset[str], int] = {
+            f_set: 0 for f_set in self.map.connections.keys()
+        }
+        """
+        """
         valid_moves: list = []
-        turn_output: deque = deque()
-
-        # =========================================================
-        # PHASE 2: THE VALIDATION (Enforcing Physics)
-        # =========================================================
         for drone in self.map.drones:
             if drone.destination and drone.state == DroneState.IN_TRANSIT:
                 drone.transit_timer -= 1
@@ -80,8 +54,6 @@ class StateEngine:
             target = moves.get(drone.name, drone.current_zone)
             if target == drone.current_zone:
                 continue
-            # validate the moves of each drone deppending on the max link
-            # of each connection
             fro_zet = frozenset([drone.current_zone, target])
             connection = self.map.connections.get(fro_zet)
             if connection:
@@ -107,18 +79,44 @@ at turn {turn}")
                 self.future_reservations[target, turn] = future_count
                 valid_moves.append((drone, target, DroneState.IN_TRANSIT))
             else:
-                # ---> THE MISSING SCENARIO E <---
-                # 1. Verify immediate capacity for Normal/Priority zones
                 if nodes_occupency[target] + 1 > zone_to_go.max_drones:
                     raise SimulationError(f"Capacity Overflow: {target}")
-                # 2. Lock in the immediate reservation!
                 nodes_occupency[target] += 1
-                # 3. YOU MUST APPEND THE MOVE so Phase 3 can execute it
                 valid_moves.append((drone, target, DroneState.ARRIVED))
+        return valid_moves
 
-        # =========================================================
-        # PHASE 3: THE COMMIT (State Update & Output formatting)
-        # =========================================================
+    def execute_turn(self, moves: dict[str, str]) -> None:
+        """
+        proposed_moves:
+        A dictionary mapping {Drone: TargetZone} for this turn.
+        If a drone is waiting, TargetZone == drone.current_zone.
+        """
+        self.turn_counter += 1
+        # this is for checking the state of the map in every turn
+        # and see the zones that have and how may drones are in there
+        nodes_occupency: dict[str, int] = {zone.name: 0
+                                           for zone in self.map.zones.values()}
+        for drone in self.map.drones:
+            if drone.state in (DroneState.ARRIVED, DroneState.WAITING):
+                nodes_occupency[drone.current_zone] += 1
+
+        # 2. Initialize CONNECTION buffer (stateless, guaranteed empty at
+        # start of turn) Using the frozenset as the key matches your
+        # network.py architecture
+        self._free_nodes_capacity(nodes_occupency, moves)
+        try:
+            valid_moves: list = self._validate_moves(nodes_occupency, moves)
+            if valid_moves:
+                self.turn_output(valid_moves)
+        except SimulationError as e:
+            print(f"Error: {e}")
+
+    # =========================================================
+    # PHASE 3: THE COMMIT (State Update & Output formatting)
+    # =========================================================
+    def turn_output(self, valid_moves: list) -> None:
+        from collections import deque
+        turn_output: deque = deque()
         for drone, destination, state in valid_moves:
             if state == DroneState.ARRIVED:
                 turn_output.append(f"{drone.name}: {drone.current_zone}\
@@ -138,61 +136,3 @@ at turn {turn}")
 
         if turn_output:
             print("\n".join(turn_output))
-
-
-"""
-import sys
-from models.parser_map import ParserMap
-def run_simulation_test() -> None:
-    print(">>> INITIALIZING MAP <<<")
-    if len(sys.argv) < 2:
-        print("no file provided")
-        return
-    try:
-        file_map = sys.argv[1]
-        parser = ParserMap()
-
-        network = parser.parser_map(filepath=file_map)
-
-        # 2. Boot the engine (which should now automatically spawn the drones)
-        engine = StateEngine(network)
-        print(">>> TURN 1: Staggering Departures <<<")
-        turn_1_moves = {
-            "D1": "waypoint1"
-            # D2 omitted. Engine fallback sets target to 'start'
-        }
-        engine.execute_turn(turn_1_moves)
-
-        print("\n>>> TURN 2: Simultaneous Shift <<<")
-        turn_2_moves = {
-            "D1": "waypoint2",
-            "D2": "waypoint1"
-        }
-        engine.execute_turn(turn_2_moves)
-
-        print("\n>>> TURN 3: First Arrival <<<")
-        turn_3_moves = {
-            "D1": "goal",
-            "D2": "waypoint2"
-        }
-        engine.execute_turn(turn_3_moves)
-
-        print("\n>>> TURN 4: Final Arrival <<<")
-        turn_4_moves = {
-            "D1": "goal",  # Explicitly commanding the wait
-            "D2": "goal"
-        }
-        engine.execute_turn(turn_4_moves)
-
-        print("\n>>> FINAL STATE VERIFICATION <<<")
-        for drone in engine.map.drones:
-            print(f"{drone.name} is {drone.state.name} \
-at {drone.current_zone}")
-    except SimulationError as e:
-        print(e)
-
-
-if __name__ == "__main__":
-    run_simulation_test()
-
-"""
